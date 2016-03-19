1673c693f9bcbe6dd6c3f1ef660a858d89024dc8
Jakub Draganek <jakub.draganek@gmail.com>
2016-03-19 15:39:11 +0100

    Solution 4: finding biggest palindrome in range of products

--- 04.lisp
+++ 04.lisp
@@ -4,6 +4,8 @@
 ;;; The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 ;;; Find the largest palindrome made from the product of two 3-digit numbers.
 
+ 
+
 (defun num-to-list (num)
  "Coverts number to list of digits"
  (coerce (prin1-to-string num) 'list))
@@ -15,7 +17,7 @@
 
 (defun list-is-palindrome (digit-list)
  "Checks list is a palindrome"
- (let* ((size     (length digit-list))
+ (let* ((      size (length digit-list))
         (slice-size (floor (/ size 2))))
      (equal (slice digit-list 1 slice-size)
             (reverse (slice digit-list (1+ (- size slice-size)) size)))))
@@ -25,3 +27,31 @@
  (list-is-palindrome (num-to-list num)))
 
 
+(defun flatten (l)
+ "Just flatten given nested list"
+ (cond ((null l) nil)
+       ((atom l) (list l))
+       (t (loop for a in l appending (flatten a)))))
+
+
+(defun all-products (max-num min-num)
+ "Produces list of all products (1 to max-num) x (1 to max-num)"
+ (flatten (loop for i from max-num downto min-num
+           collect (flet ((multiplicate (x) (* x i)))
+                    (mapcar #'multiplicate
+                            (loop
+                             for k from max-num downto i
+                             collect k)))))) 
+
+
+(defun find-biggest-palindrome (number-seq)
+ "Finds biggest of palindromes in unordered number-seq"
+ (find-if #'palindrome? (sort number-seq #'>)))
+
+
+;;; MAIN function for executable
+
+(defun main()
+ (time (let ((   big 999)
+             ( small 100))
+      (print (find-biggest-palindrome (all-products big small))))))
