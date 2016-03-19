db12131e787b9d5794af94e6567c58442b99fd18
Jakub Draganek <jakub.draganek@gmail.com>
2016-03-19 15:39:32 +0100

    tuning executables a bit

--- build.lisp
+++ build.lisp
@@ -1,8 +1,19 @@
 (defvar *script-name* (nth 1 *posix-argv*))
 (defvar *output-file* (nth 2 *posix-argv*))
 
+;;; Optimization policy
+(proclaim
+ '(optimize
+   (compilation-speed 0) (debug 1) (inhibit-warnings 1)
+   (safety 1) (space 0) (speed 3)))
+
+;;; Load Lisp file to build
 (load *script-name*)
 
+;;; Make executable
 (sb-ext:save-lisp-and-die *output-file*
+ :purify t
  :executable t
  :toplevel 'main)
+    
+
