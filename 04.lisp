bf9b2e3c95cf12e75eb61f0cfd08c8853c209c07
Jakub Draganek <jakub.draganek@gmail.com>
2016-03-19 14:50:47 +0100

    whitespace align

--- 04.lisp
+++ 04.lisp
@@ -5,8 +5,8 @@
 ;;; Find the largest palindrome made from the product of two 3-digit numbers.
 
 (defun num-to-list (num)
-    "Coverts number to list of digits"
-    (coerce (prin1-to-string num) 'list))
+ "Coverts number to list of digits"
+ (coerce (prin1-to-string num) 'list))
 
 (defun slice (lst begin end)
  "Gets slice from list [begin,end] when indexes go from 1 to legth of lst"
@@ -14,13 +14,14 @@
              (min (length lst) end)))
 
 (defun list-is-palindrome (digit-list)
-    "Checks list is a palindrome"
-    (let* ((size     (length digit-list))
-           (slice-size (floor (/ size 2))))
-        (equal (slice digit-list 1 slice-size)
-               (reverse (slice digit-list (1+ (- size slice-size)) size)))))
+ "Checks list is a palindrome"
+ (let* ((size     (length digit-list))
+        (slice-size (floor (/ size 2))))
+     (equal (slice digit-list 1 slice-size)
+            (reverse (slice digit-list (1+ (- size slice-size)) size)))))
 
 (defun palindrome? (num)
  "Checks if number is a palindrome"
  (list-is-palindrome (num-to-list num)))
+
 
